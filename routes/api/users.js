15211f40f79c379bde2ca5ef73846059a3f25cdc
Zane Campbell <zanegitinit@gmail.com>
2019-08-10 11:27:02 -0700

    setup registration route, returns jsonwebtoken on success

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,3 +4,77 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
+
+// @route POST api/users
+// @desc Register new user
+// @access Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      // See if email is already is use
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Email already in use' }] });
+      }
+
+      // Encrpyt password
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      // Hash the password 10x (recommended amount)
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 604800 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+module.exports = router;
