55cfa8de6a2596532e68593934c09516bdb85af5
Zane Campbell <zanegitinit@gmail.com>
2019-08-11 16:05:22 -0700

    added authentication and jsonwebtoken Login route and added authMiddleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -77,4 +77,63 @@ router.post(
   }
 );
 
+// @route GET api/users
+// @desc Authenticate user & get token (Login)
+// @access Public
+router.get(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      // See if user exists
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials ' }] });
+      }
+
+      // Check if user and password match by caparing req.body.password and user.password from the db
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      // Return jsonwebtoken if everything matches
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 604800 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
