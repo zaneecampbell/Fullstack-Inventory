db635c64a59865c02cef3c459debe87fe13a8b4d
Zane Campbell <zanegitinit@gmail.com>
2019-08-23 21:00:09 -0700

    setup basic form and redux request for login

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
+} from './types';
 
 // Register User
 export const register = ({ name, email, password }) => async dispatch => {
@@ -21,6 +26,42 @@ export const register = ({ name, email, password }) => async dispatch => {
   } catch (err) {
     dispatch({
       type: REGISTER_FAIL
+    });
+  }
+};
+
+// Login User
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  console.log('test');
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.get('/api/users', body, config);
+
+    dispatch(
+      {
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      },
+      console.log('success')
+    );
+
+    // dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(console.log(error.msg)));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL
     });
   }
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,93 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import Paper from '@material-ui/core/Paper';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import { login } from '../actions/auth';
 
-const Login = () => {
-  return <div />;
+const Login = ({ login }) => {
+  const [loginData, setLoginData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = loginData;
+
+  const onChange = e => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    localStorage.removeItem('token');
+    e.preventDefault();
+    login(email, password);
+  };
+
+  return (
+    <Fragment>
+      <Paper
+        style={{
+          maxWidth: '750px',
+          margin: 'auto',
+          marginTop: '50px',
+          padding: '10px',
+          paddingBottom: '30px',
+          textAlign: 'center'
+        }}
+      >
+        <form
+          style={{ marginTop: '50px', marginBottom: '50px' }}
+          onSubmit={e => onSubmit(e)}
+        >
+          <Input
+            style={{
+              fontSize: '50px',
+              margin: '15px',
+              marginBottom: '50px'
+            }}
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+          <Input
+            style={{
+              fontSize: '50px',
+              margin: '15px',
+              marginBottom: '50px'
+            }}
+            type='password'
+            value={password}
+            name='password'
+            onChange={e => onChange(e)}
+            placeholder='Password'
+            autoComplete='off'
+          />
+          <Button
+            style={{
+              marginTop: '25px',
+              fontSize: '30px',
+              backgroundColor: '#3f51b5',
+              padding: '15px',
+              color: 'white'
+            }}
+            type='submit'
+          >
+            Login
+          </Button>
+        </form>
+      </Paper>
+    </Fragment>
+  );
 };
 
-export default Login;
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
