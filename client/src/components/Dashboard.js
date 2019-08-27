bdb90f1f8675d180993517cf13ccfc94fa447cb1
Zane Campbell <zanegitinit@gmail.com>
2019-08-27 14:11:19 -0700

    began working on registration page

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
---
+++ client/src/components/Register.js
@@ -0,0 +1,114 @@
+import React, { Fragment, useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Paper from '@material-ui/core/Paper';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import { register } from '../actions/auth';
+
+const Register = ({ register, isAuthenticated }) => {
+  const [registerData, setRegisterData] = useState({
+    name,
+    email: '',
+    password: ''
+  });
+
+  const { name, email, password } = registerData;
+
+  const onChange = e => {
+    setLoginData({ ...registerData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    register(name, email, password);
+  };
+
+  if (isAuthenticated) {
+    alert(
+      'You are already registered, please logout to register a new account'
+    );
+    return <Redirect to='/dashboard' />;
+  }
+
+  return (
+    <Fragment>
+      <Paper
+        style={{
+          maxWidth: '750px',
+          margin: 'auto',
+          marginTop: '50px',
+          padding: '10px',
+          paddingBottom: '30px',
+          textAlign: 'center'
+        }}
+      >
+        <form
+          style={{ marginTop: '50px', marginBottom: '50px' }}
+          onSubmit={e => onSubmit(e)}
+        >
+          <Input
+            style={{
+              fontSize: '50px',
+              margin: '15px',
+              marginBottom: '50px'
+            }}
+            type='name'
+            placeholder='Your Name'
+            name='name'
+            value={name}
+            onChange={e => onChange(e)}
+            required
+          />
+          <Input
+            style={{
+              fontSize: '50px',
+              margin: '15px',
+              marginBottom: '50px'
+            }}
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+          <Input
+            style={{
+              fontSize: '50px',
+              margin: '15px',
+              marginBottom: '50px'
+            }}
+            type='password'
+            value={password}
+            name='password'
+            onChange={e => onChange(e)}
+            placeholder='Password'
+            autoComplete='off'
+          />
+          <Button
+            style={{
+              marginTop: '25px',
+              fontSize: '30px',
+              backgroundColor: '#3f51b5',
+              padding: '15px',
+              color: 'white'
+            }}
+            type='submit'
+          >
+            Register
+          </Button>
+        </form>
+      </Paper>
+    </Fragment>
+  );
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
