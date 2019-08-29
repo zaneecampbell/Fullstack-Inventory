a99389c23c387717fdf8c169964aba0771178c57
Zane Campbell <zanegitinit@gmail.com>
2019-08-28 17:43:58 -0700

    began working on inventory routes in redux

---
+++ client/src/actions/inventory.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+// import { setAlert } from './alert';
+
+import { GET_INVENTORY, INVENTORY_ERROR, UPDATE_INVENTORY } from './types';
+
+// Get current users inventory
+export const getCurrentInventory = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/inventory');
+
+    dispatch({
+      type: GET_INVENTORY,
+      payload: res.data
+    });
+  } catch (err) {
+    if (errors) {
+      console.log(errors);
+    }
+
+    dispatch({
+      type: INVENTORY_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Create or update inventory
+export const createInventory = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const res = await axios.post('/api/inventory', formData, config);
+
+    dispatch({
+      type: GET_INVENTORY,
+      payload: res.data
+    });
+
+    // dispatch(setAlert(edit ? 'Inventory Updated' : 'Inventory Created', 'success'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      console.log(errors);
+    }
+
+    dispatch({
+      type: INVENTORY_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
+import inventory from './inventory';
 
-export default combineReducers({ auth });
+export default combineReducers({ auth, inventory });
