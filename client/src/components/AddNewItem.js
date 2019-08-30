f5e6de262b163447edaf6cd1f20f78590143115a
Zane Campbell <zanegitinit@gmail.com>
2019-08-30 11:23:31 -0700

    made it so if youre logged in keeps you on current page during refresh

---
+++ client/src/components/AddNewItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddNewItem = () => {
+  return <div></div>;
+};
+
+export default AddNewItem;
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,40 +2,44 @@ import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getCurrentInventory } from '../actions/inventory';
+import AddNewItem from './AddNewItem';
 
 const Dashboard = ({
   getCurrentInventory,
   isAuthenticated,
+  authLoading,
   inventory: { inventory, loading }
 }) => {
   useEffect(() => {
     getCurrentInventory();
   }, [getCurrentInventory]);
 
-  if (!isAuthenticated) {
+  if (!isAuthenticated && !authLoading) {
     return <Redirect to='/' />;
   }
 
   return loading && inventory === null ? (
-    <h1>Loading Your Inventory</h1>
+    <h1>Loading Your Inventory...</h1>
   ) : (
     <div>
       <h1>Your Inventory</h1>
       {inventory !== false ? (
         inventory.map(item => (
-          <div>
+          <div key={item.index}>
             {item.item}: {item.amount}
           </div>
         ))
       ) : (
         <div>Your inventory is empty</div>
       )}
+      <AddNewItem />
     </div>
   );
 };
 
 const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated,
+  authLoading: state.auth.loading,
   inventory: state.inventory
 });
 
