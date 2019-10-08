65ec764b931144f8b024305f7dfdf08136b4476d
Zane Campbell <zanegitinit@gmail.com>
2019-10-07 18:53:47 -0700

    working on what we do section

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { login } from '../actions/auth';
-import { Typography } from '@material-ui/core';
+import { Typography, CardContent } from '@material-ui/core';
 
 const Login = ({ login, isAuthenticated }) => {
   if (isAuthenticated) {
@@ -61,7 +63,7 @@ const Login = ({ login, isAuthenticated }) => {
           </div>
         </div>
       </div>
-      <div style={{ background: 'white', textAlign: 'left' }}>
+      <div style={{ background: 'white', textAlign: 'center' }}>
         <div
           style={{
             marginLeft: '50px',
@@ -69,17 +71,90 @@ const Login = ({ login, isAuthenticated }) => {
             paddingTop: '25px'
           }}
         >
+          {' '}
+          <Typography style={{ fontWeight: 'bold' }} variant='h2'>
+            What we do
+          </Typography>
+          <div style={{margin:'auto'}}>
+          <Grid
+            container
+            justify='center'
+            style={{ marginTop: '30px' }}
+          >
+            <Grid
+              xs={12}
+              md={3}
+              style={{ marginLeft: '20px', marginRight: '20px' }}
+              item
+            >
+              <Card style={{boxShadow: 'none', marginBottom: '20px'}}>
+                <CardContent>
+                  <img src="/images/database.png" alt="database icon" />
+                  <Typography style={{marginTop: '10px'}}>
+                    Save your information in our database.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid
+              xs={12}
+              md={3}
+              style={{ marginLeft: '20px', marginRight: '20px' }}
+              item
+            >
+              <Card style={{boxShadow: 'none', marginBottom: '20px'}}>
+                <CardContent>
+                <img src="/images/key.png" alt="key icon" />
+                  <Typography>Allow you to login from anywhere</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid
+              xs={12}
+              md={3}
+              style={{ marginLeft: '20px', marginRight: '20px' }}
+              item
+            >
+              <Card style={{boxShadow: 'none', marginBottom: '20px'}}>
+                <CardContent>
+                  <Typography>Keep things simple and clean.</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          background: 'white',
+          textAlign: 'left',
+          marginLeft: '50px',
+          marginRight: '50px'
+        }}
+      >
+        <div
+          style={{
+            margin: 'auto',
+            paddingTop: '25px',
+            maxWidth: '1600px'
+          }}
+        >
           <Typography style={{ fontWeight: 'bold' }} variant='h2'>
             Why choose us?
           </Typography>
           <Typography
-            style={{ marginTop: '10px', paddingBottom: '25px' }}
+            style={{
+              marginTop: '10px',
+              paddingBottom: '25px'
+            }}
             variant='h5'
           >
             Snappy Inventory isn't here to bog you down with too much for your
             fundraiser, charity event, or pokemon GO meetup (hey we dont judge).
             We're a small quicky (snappy if you will) inventory management
-            system trying to keep things simple.
+            system trying to keep things simple for your inventory tracking
+            needs.
           </Typography>
         </div>
       </div>
