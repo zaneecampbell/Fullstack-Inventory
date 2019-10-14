3560c9275134c9fc9f230af597bc0abf6859f52f
Zane Campbell <zanegitinit@gmail.com>
2019-10-13 18:29:47 -0700

    fixed issue with background being broke if your form was too long

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -81,115 +81,115 @@ const Dashboard = ({
     </div>
   ) : (
     <div
-      style={{
-        background: 'url(images/warehouse.jpg)',
-        backgroundRepeat: 'no-repeat',
-        backgroundAttachment: 'fixed',
-        backgroundSize: 'cover',
-        height: `calc(100vh - 64px)`
-      }}
+      style={
+        {
+          // background: 'url(images/warehouse.jpg)',
+          // backgroundRepeat: 'no-repeat',
+          // backgroundAttachment: 'scroll',
+          // backgroundSize: 'cover',
+          // height: `calc(100vh - 64px)`
+        }
+      }
     >
-      <Fragment>
-        <div style={{ height: '50px' }}></div>
-        <Paper
-          style={{
-            maxWidth: '750px',
-            margin: 'auto',
-            padding: '10px',
-            paddingBottom: '30px',
-            textAlign: 'center'
-          }}
-        >
-          {user !== null ? (
-            <Typography variant='h3' style={{ marginBottom: '20px' }}>
-              {user.name}'s Inventory
-            </Typography>
-          ) : (
-            <Typography variant='h3' style={{ marginBottom: '20px' }}>
-              Your Inventory
-            </Typography>
-          )}
-          {alerts.map((alert, idx) => (
-            <Typography
-              style={{ background: 'red', color: 'white', fontSize: '22px' }}
-              key={idx}
-            >
-              {alert.msg}
-            </Typography>
-          ))}
-          <AddNewItem />
-          {inventory !== null && loading === false ? (
-            <form onSubmit={e => onSubmit(e)}>
-              {inventoryArray.map((item, idx) => (
-                <Grid container justify='center' key={item.index}>
-                  <Typography variant='h5'>
-                    <Grid style={{ textAlign: 'left' }} xs={12} item>
-                      {item.item}:
-                    </Grid>
-                    <Grid xs={12} item>
-                      <input
-                        style={{
-                          fontSize: '22px',
-                          marginTop: '15px',
-                          marginBottom: '15px',
-                          marginRight: '15px',
-                          maxWidth: '100px'
-                        }}
-                        type='number'
-                        id={`${idx}`}
-                        placeholder='Enter Amount'
-                        name='amount'
-                        value={item.amount}
-                        onChange={e => onChange(e)}
-                      ></input>
-                      <Button
-                        style={{
-                          fontSize: '16px',
-                          backgroundColor: 'rgb(147, 25, 37)',
-                          padding: '5px',
-                          color: 'white'
-                        }}
-                        onClick={e => onDelete(e, item.index)}
-                      >
-                        X
-                      </Button>
-                    </Grid>
-                  </Typography>
-                </Grid>
-              ))}
-              <Button
-                style={{
-                  marginTop: '25px',
-                  marginBottom: '5px',
-                  fontSize: '30px',
-                  backgroundColor: '#3f51b5',
-                  padding: '15px',
-                  color: 'white'
-                }}
-                type='submit'
-              >
-                Update Inventory
-              </Button>
-            </form>
-          ) : (
-            <div>
-              {alerts.map((alert, idx) => (
-                <Typography
-                  style={{
-                    background: 'red',
-                    color: 'white',
-                    fontSize: '22px'
-                  }}
-                  key={idx}
-                >
-                  {alert.msg}
+      <div style={{ height: '50px' }}></div>
+      <Paper
+        style={{
+          maxWidth: '750px',
+          margin: 'auto',
+          padding: '10px',
+          paddingBottom: '30px',
+          textAlign: 'center'
+        }}
+      >
+        {user !== null ? (
+          <Typography variant='h3' style={{ marginBottom: '20px' }}>
+            {user.name}'s Inventory
+          </Typography>
+        ) : (
+          <Typography variant='h3' style={{ marginBottom: '20px' }}>
+            Your Inventory
+          </Typography>
+        )}
+        {alerts.map((alert, idx) => (
+          <Typography
+            style={{ background: 'red', color: 'white', fontSize: '22px' }}
+            key={idx}
+          >
+            {alert.msg}
+          </Typography>
+        ))}
+        <AddNewItem />
+        {inventory !== null && loading === false ? (
+          <form onSubmit={e => onSubmit(e)}>
+            {inventoryArray.map((item, idx) => (
+              <Grid container justify='center' key={item.index}>
+                <Typography variant='h5'>
+                  <Grid style={{ textAlign: 'left' }} xs={12} item>
+                    {item.item}:
+                  </Grid>
+                  <Grid xs={12} item>
+                    <input
+                      style={{
+                        fontSize: '22px',
+                        marginTop: '15px',
+                        marginBottom: '15px',
+                        marginRight: '15px',
+                        maxWidth: '100px'
+                      }}
+                      type='number'
+                      id={`${idx}`}
+                      placeholder='Enter Amount'
+                      name='amount'
+                      value={item.amount}
+                      onChange={e => onChange(e)}
+                    ></input>
+                    <Button
+                      style={{
+                        fontSize: '16px',
+                        backgroundColor: 'rgb(147, 25, 37)',
+                        padding: '5px',
+                        color: 'white'
+                      }}
+                      onClick={e => onDelete(e, item.index)}
+                    >
+                      X
+                    </Button>
+                  </Grid>
                 </Typography>
-              ))}
-              <h3>Your inventory is empty</h3>
-            </div>
-          )}
-        </Paper>
-      </Fragment>
+              </Grid>
+            ))}
+            <Button
+              style={{
+                marginTop: '25px',
+                marginBottom: '5px',
+                fontSize: '30px',
+                backgroundColor: '#3f51b5',
+                padding: '15px',
+                color: 'white'
+              }}
+              type='submit'
+            >
+              Update Inventory
+            </Button>
+          </form>
+        ) : (
+          <div>
+            {alerts.map((alert, idx) => (
+              <Typography
+                style={{
+                  background: 'red',
+                  color: 'white',
+                  fontSize: '22px'
+                }}
+                key={idx}
+              >
+                {alert.msg}
+              </Typography>
+            ))}
+            <h3>Your inventory is empty</h3>
+          </div>
+        )}
+      </Paper>
     </div>
   );
 };
@@ -206,5 +206,3 @@ export default connect(
   mapStateToProps,
   { getCurrentInventory, inventoryUpdate, deleteItem }
 )(Dashboard);
-
-// Add alert for update confirmation
